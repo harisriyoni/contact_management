36a7f4e1d0785f3c532b335e4b66f23e93f7a4b2
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-31 15:27:22 +0700

    add relasi

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Contact extends Model
 {
@@ -16,5 +17,9 @@ class Contact extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(Address::class, "contact_id", "id");
     }
 }
