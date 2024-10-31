148125e5cada5d6e3ab5ffcec0bd119d096601a3
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-10-31 15:06:58 +0700

    relasi model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Contact extends Model
 {
@@ -13,4 +13,8 @@ class Contact extends Model
     public $incrementing = true;
     public $timestamps = true;
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
@@ -12,5 +12,10 @@ class User extends Model
     protected $keyType = "int";
     public $incrementing = true;
     public $timestamps = true;
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class, 'users_id', 'id');
+    }
 
 }
